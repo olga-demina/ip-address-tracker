05d6d23658b73989e81152f979e2a470fd58481f
olga-demina <olga.demina1410@gmail.com>
2022-10-24 08:27:23 +0200

    Add: Loading user location on page initialization

--- src/App.vue
+++ src/App.vue
@@ -1,53 +1,34 @@
 <script setup>
-import HelloWorld from "./components/HelloWorld.vue";
-import TheWelcome from "./components/TheWelcome.vue";
+import { onMounted, reactive } from 'vue';
+
+const state = reactive({
+  ip: "",
+  location: {}
+});
+
+onMounted(() => {
+  getUserIp();
+});
+
+async function getUserIp() {
+  const params = {
+    apiKey: "at_g3naFAGd8HRTmapbconMMbXQqODZV",
+    ipAddress: ""
+  }
+  const url = new URL("https://geo.ipify.org/api/v2/country,city");
+  for (let key in params) {
+    url.searchParams.append(key, params[key]);
+  }
+  const ipData = await (await fetch(url)).json();
+  state.ip = ipData.ip;
+  state.location = ipData.location;
+}
+
 </script>
 
 <template>
-  <header>
-    <img
-      alt="Vue logo"
-      class="logo"
-      src="./assets/logo.svg"
-      width="125"
-      height="125"
-    />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
+<style scoped lang="scss">
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
